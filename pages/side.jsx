be8a74059d174855577a83a15babf68ca28a07e0
c <liming.cui@electrifai.com>
2023-08-31 04:49:55 +0800

    update

--- pages/side.jsx
+++ pages/side.jsx
@@ -1,15 +1,14 @@
-import React from 'react';
-
+import React from "react";
 
 const Widget = ({ title, imageUrl }) => (
   <div className="bg-gray-100 p-2">
-    <p className="text-center mt-2">{title}</p>
-    <img src={require('../data/pics/pic.jpg')} className="w-full" />
+    <p className="mt-2 text-center">{title}</p>
+    <img src="/static/images/google.png" className="w-full" />
   </div>
 );
 
 const DataDisplay = ({ data }) => (
-  <div className="flex flex-wrap -mx-2">
+  <div className="-mx-2 flex flex-wrap">
     {data.map((item, index) => (
       <div key={index} className="">
         <Widget title={item.title} imageUrl={item.imageUrl} />
@@ -20,24 +19,24 @@ const DataDisplay = ({ data }) => (
 
 const SideWidget = () => {
   const widgetData = [
-    { title: 'Title 1', imageUrl: '/pics/pic.jpg' },
-    { title: 'Title 2', imageUrl: '/pics/pic.jpg' },
-    { title: 'Title 3', imageUrl: '/pics/pic.jpg' },
-    { title: 'Title 4', imageUrl: '/pics/pic.jpg' }
+    { title: "Title 1", imageUrl: "/pics/pic.jpg" },
+    { title: "Title 2", imageUrl: "/pics/pic.jpg" },
+    { title: "Title 3", imageUrl: "/pics/pic.jpg" },
+    { title: "Title 4", imageUrl: "/pics/pic.jpg" },
   ];
 
   const listData = [
-    { title: 'List Item 1', imageUrl: 'listImage1.jpg' },
-    { title: 'List Item 2', imageUrl: 'listImage2.jpg' },
-    { title: 'List Item 3', imageUrl: 'listImage3.jpg' },
-    { title: 'List Item 4', imageUrl: 'listImage4.jpg' },
+    { title: "List Item 1", imageUrl: "listImage1.jpg" },
+    { title: "List Item 2", imageUrl: "listImage2.jpg" },
+    { title: "List Item 3", imageUrl: "listImage3.jpg" },
+    { title: "List Item 4", imageUrl: "listImage4.jpg" },
   ];
 
   return (
     <div className="flex items-center justify-center pt-4">
-    <div>
-    <DataDisplay data={widgetData}/>
-    </div>  
+      <div>
+        <DataDisplay data={widgetData} />
+      </div>
     </div>
   );
 };
