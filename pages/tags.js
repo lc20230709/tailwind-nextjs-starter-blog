a9227ea87dc237b3bc5dc356cfb5310bfa8d3f39
c <liming.cui@electrifai.com>
2023-09-11 03:58:06 +0800

    update

--- pages/tags.js
+++ pages/tags.js
@@ -22,7 +22,7 @@ export default function Tags({ tags }) {
         title={`标签 - ${keys}`}
         description={`感兴趣的标签内容${keys}`}
       />
-      <div className="flex flex-col items-start justify-start divide-y divide-gray-200 dark:divide-gray-700 md:mt-24 md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
+      <div className="flex flex-col items-start justify-start  md:flex-row md:items-center md:justify-center md:space-x-6 md:divide-y-0">
         <div className="flex max-w-lg flex-wrap">
           {Object.keys(tags).length === 0 && "No tags found."}
           {sortedTags.map((t) => {
